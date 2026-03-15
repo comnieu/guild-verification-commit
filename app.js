32d2829e88d48d3d9d0938ca18d4664ddb061a2d
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:06:10 +0700

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:06:07.262Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-15T23:06:08.973Z
+Commit: Fix memory leak in event handlers
