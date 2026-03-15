b1d9dad4b940907e8611dc0c25f49f7036bbec53
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:06:21 +0700

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:06:18.494Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-15T23:06:20.180Z
+Commit: Fix memory leak in event handlers
