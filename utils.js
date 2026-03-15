5de931c9e5fd4f51390b699431861d2276b0cd13
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:06:18 +0700

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:05:57.491Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-15T23:06:17.046Z
+Commit: Fix memory leak in event handlers
