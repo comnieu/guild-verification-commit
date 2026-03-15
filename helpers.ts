a504cef0a31a975d65700cdd064ea51b305a9013
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:10:02 +0700

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:09:10.799Z
-Commit: Add dark mode support
+Timestamp: 2026-03-15T23:10:01.310Z
+Commit: Resolve null pointer exception in user service
