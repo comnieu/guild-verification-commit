eecaf57209cf8a2559ea58e41d354787279d5e22
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:08:18 +0700

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:08:13.436Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-15T23:08:16.772Z
+Commit: Patch API endpoint error handling
