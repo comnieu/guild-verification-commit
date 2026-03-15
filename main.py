94adb81d944c73f662deca541d51cfd8a9dae335
comnieu <129941743+comnieu@users.noreply.github.com>
2026-03-16 06:08:58 +0700

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T23:08:16.772Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-15T23:08:56.754Z
+Commit: Fix memory leak in event handlers
